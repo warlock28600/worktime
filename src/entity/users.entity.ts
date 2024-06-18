a1e0416c3156a93507c1cde32d06aa854aa1ff44
farhang <farhang.ezziirani@gmail.com>
2024-06-18 20:59:16 +0330

    some jobs

--- src/entity/users.entity.ts
+++ src/entity/users.entity.ts
@@ -1,13 +1,14 @@
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { PersonEntity } from './person.entity';
+import { IsNumber } from 'class-validator';
 
 @Entity()
 export class UsersEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'timestamp' })
-  expireDate: Date;
+  @IsNumber()
+  expireDate: number;
 
   @Column()
   passWord: string;
