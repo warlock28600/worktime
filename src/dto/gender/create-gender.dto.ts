c379ebb941e769d2319a5d3a2640caeac379d83f
Farhang <farhang.ezziirani@gmail.com>
2024-06-22 00:48:38 +0330

    some jobs

--- src/dto/gender/create-gender.dto.ts
+++ src/dto/gender/create-gender.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator';
+import {IsString} from 'class-validator';
+import {ApiProperty} from "@nestjs/swagger";
 
 
 export class CreateGenderDto{
 
   @IsString()
+  @ApiProperty()
   title:string
 
 }
