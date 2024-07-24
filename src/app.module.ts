fd6f9bf565f5d78310120c097ccfe56f765f32ee
Farhang <farhang.ezziirani@gmail.com>
2024-07-24 15:13:06 -0700

    some jobs

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,13 +10,23 @@ import { UsersModule } from './users/users.module';
 import { UsersEntity } from './entity/users.entity';
 import { UnitsModule } from './units/units.module';
 import { UnitEntity } from './entity/unit.entity';
+import { PersonInUnitEntity } from './entity/person-in-unit.entity';
 
 @Module({
   imports: [PersonModule,TypeOrmModule.forRoot({
-    type:'sqlite',
-    database:'workTime.sqlite',
-    entities: [PersonEntity, GenderEntity, UsersEntity, UnitEntity],
-    synchronize:true
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'F.ei28600',
+    database: 'WorkTime',
+    synchronize: true,
+    logging: true,
+    entities: [PersonEntity, GenderEntity, UsersEntity, UnitEntity, PersonInUnitEntity],
+    // type:'sqlite',
+    // database:'workTime.sqlite',
+    // entities: [PersonEntity, GenderEntity, UsersEntity, UnitEntity,PersonInUnitEntity],
+    // synchronize:true
   }), GenderModule, UsersModule, UnitsModule],
   controllers: [AppController],
   providers: [AppService],
